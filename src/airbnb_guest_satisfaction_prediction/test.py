53c14a78dcea0a2d588e60b6733133b237285f6e
expl0r3rgu1 <salvaddimando@gmail.com>
2023-05-10 20:12:56 +0200

    testing

--- src/airbnb_guest_satisfaction_prediction/test.py
+++ src/airbnb_guest_satisfaction_prediction/test.py
@@ -7,9 +7,10 @@ import pandas as pd
 import os.path
 import kaggle
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler, PolynomialFeatures
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, PolynomialFeatures
 from sklearn.pipeline import Pipeline
-from sklearn.linear_model import Ridge, Lasso
+from sklearn.linear_model import LinearRegression, Ridge, Lasso, ElasticNet
+from sklearn.kernel_ridge import KernelRidge
 from sklearn.metrics import mean_squared_error, r2_score
 
 
@@ -68,9 +69,13 @@ for column in dataset.columns.drop("Guest Satisfaction"):
     dataset.plot.scatter(x=column, y="Guest Satisfaction").get_figure().savefig(
         "plots/" + column + ".png")
 
-# Columns to be removed: Bedrooms, Business, City, Day, Multiple Rooms, Attraction Index, Restraunt Index, Person Capacity, Private Room, Room Type, Shared Room, Normalised Attraction Index, Normalised Restraunt Index
-dataset = dataset.drop(columns=["Bedrooms", "Business", "City", "Day", "Multiple Rooms", "Attraction Index", "Restraunt Index",
-                       "Person Capacity", "Private Room", "Room Type", "Shared Room", "Normalised Attraction Index", "Normalised Restraunt Index"])
+# Columns to be removed: Bedrooms, Business, City, Day, Multiple Rooms, Person Capacity, Private Room, Room Type, Shared Room, Normalised Attraction Index, Normalised Restraunt Index
+dataset = dataset.drop(columns=["Bedrooms", "Business", "City", "Day", "Multiple Rooms", "Person Capacity",
+                       "Private Room", "Room Type", "Shared Room", "Normalised Attraction Index", "Normalised Restraunt Index"])
+
+# Remaining columns
+print("Remaining columns")
+print(dataset.columns.values)
 
 # Separating training and test data with a 80:20 ratio in two different datasets
 train_set, test_set = train_test_split(dataset, test_size=0.2, random_state=42)
@@ -80,33 +85,57 @@ y_train = train_set["Guest Satisfaction"]
 x_test = test_set.drop(columns=["Guest Satisfaction"])
 y_test = test_set["Guest Satisfaction"]
 
+# Testing Linear Regression
+print("Linear Regression")
+model_poli_std_lin = Pipeline([
+    ('poly', PolynomialFeatures(degree=3, include_bias=False)),
+    ('scaler', StandardScaler()),
+    ('linear', LinearRegression())
+], verbose=True)
+
+model_poli_std_lin.fit(x_train, y_train)
+
+print_eval(x_train, y_train, model_poli_std_lin)
+
+print_eval(x_test, y_test, model_poli_std_lin)
+
 # Testing Ridge Regression
 print("Ridge Regression")
-model = Pipeline([
-    ('poly', PolynomialFeatures(degree=10, include_bias=False)),
+model_poli_std_rdg = Pipeline([
+    ('poly', PolynomialFeatures(degree=3, include_bias=False)),
     ('scaler', StandardScaler()),
     ('ridge', Ridge(alpha=1))
-])
+], verbose=True)
 
-model.fit(x_train, y_train)
+model_poli_std_rdg.fit(x_train, y_train)
 
-print_eval(x_train, y_train, model)
+print_eval(x_train, y_train, model_poli_std_rdg)
 
-print_eval(x_test, y_test, model)
+print_eval(x_test, y_test, model_poli_std_rdg)
 
-# plot_predictions(x_train, y_train, model)
-# pd.Series(model.named_steps["ridge"].coef_, x_train.columns)
-
-# Testing Lasso Regression
-print("Lasso Regression")
-model = Pipeline([
+# Testing Elastic Net Regression
+print("Elastic Net Regression")
+model_poli_std_en = Pipeline([
+    ('poly', PolynomialFeatures(degree=10, include_bias=False)),
     ('scaler', StandardScaler()),
-    ('lasso', Lasso(alpha=0.1))
-])
+    ('elasticnet', ElasticNet(alpha=0.1, l1_ratio=0.8))
+], verbose=True)
 
-model.fit(x_train, y_train)
+model_poli_std_en.fit(x_train, y_train)
 
-print_eval(x_train, y_train, model)
+print_eval(x_train, y_train, model_poli_std_en)
 
-# plot_predictions(x_train, y_train, model)
-pd.Series(model.named_steps["lasso"].coef_, x_train.columns)
+print_eval(x_test, y_test, model_poli_std_en)
+
+# Testing KernelRidge Regression
+# print("KernelRidge Regression")
+# model_poli_std_krr = Pipeline([
+#     ('scaler', StandardScaler()),
+#     ('krr', KernelRidge(alpha=1, kernel='poly', degree=10))
+# ], verbose=True)
+
+# model_poli_std_krr.fit(x_train, y_train)
+
+# print_eval(x_train, y_train, model_poli_std_krr)
+
+# print_eval(x_test, y_test, model_poli_std_krr)
